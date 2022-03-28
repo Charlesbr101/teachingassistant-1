a1c273067bc95b7031317eb4792caefa0dfbfba4
Charles Matheus <cmsa@cin.ufpe.br>
2022-03-28 14:31:46 -0300

    Cherry pick cadastro de metas, e clonagem de objetos para simular funcionamento do servidor

--- ta-gui/src/app/aluno.ts
+++ ta-gui/src/app/aluno.ts
@@ -1,7 +1,34 @@
 export class Aluno {
-  nome: string = "";
-  cpf: string = "";
-  email: string = "";
+  nome!: string;
+  cpf!: string;
+  email!: string;
+  metas!: Map<string,string>;
 
-  constructor() {}
+  constructor() {
+    this.clean();
+  }
+
+  clean(): void {
+    this.nome = "";
+    this.cpf = "";
+    this.email = "";
+    this.metas = new Map<string,string>();
+  }
+
+  clone(): Aluno {
+    var aluno: Aluno = new Aluno();
+    aluno.nome = this.nome;
+    aluno.cpf = this.cpf;
+    aluno.email = this.email;
+    aluno.metas = this.cloneMetas();
+    return aluno;
+  }
+
+  cloneMetas(): Map<string,string> {
+    var metas: Map<string,string> = new Map<string,string>();
+    for (let entry of this.metas) {
+      metas.set(entry[0], entry[1]);
+    }
+    return metas;
+  }
 }
